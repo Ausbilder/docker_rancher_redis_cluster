edbaa3e6b3705ea3677628ea32a90960b9bd05d3
Ausbilder <sven.ahlfeld@gmx.net>
2016-07-05 09:06:55 +0200

    fix

--- conf/getContainerListForService.sh
+++ conf/getContainerListForService.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #declare a dictionary Hostname:IP
 declare -A CONTAINER
-container_map=$(curl http://rancher-metadata/latest/services/"$1"/containers)	
+container_map=$(curl http://rancher-metadata/latest/services/$1/containers)	
 for host in $container_map;
 do
-	container_name=${$host#*=}
+	index=`expr index "$host" =`
+	container_name=${host:index+1}
 	container_ip=$(curl http://rancher-metadata/latest/containers/$container_name/primary_ip)
 	CONTAINER+=(["$container_name"]="$container_ip")
 done
